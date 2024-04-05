876debf365f4cdb5589e5a09d5cc24ac4f9ea268
webthethird <ratzbodell@gmail.com>
2024-04-04 20:23:33 -0400

    Implement `resolve`, add modifiers

--- src/TruthyMarket.sol
+++ src/TruthyMarket.sol
@@ -14,9 +14,20 @@ contract TruthyMarket is IBinaryOutcomeMarket, Ownable {
     OutcomeToken[2] private _outcomes;
     address private _resolver;
     bool private _isResolved;
+    bool private _resolvedTo;
 
     modifier onlyResolver() {
         require(_msgSender() == _resolver, "Only the resolver can call this function");
+        _;
+    }
+
+    modifier notResolved() {
+        require(!_isResolved, "Market already resolved");
+        _;
+    }
+
+    modifier validIndex(uint256 idx) {
+        require(idx < 2, "Index out of bounds");
         _;
     }
 
@@ -63,16 +74,17 @@ contract TruthyMarket is IBinaryOutcomeMarket, Ownable {
         return _resolver;
     }
 
-    function getOutcomeToken(uint256 idx) external view returns (IOutcomeToken) {
+    function getOutcomeToken(uint256 idx) external view validIndex(idx) returns (IOutcomeToken) {
         return _outcomes[idx];
     }
 
-    function getOutcomePrice(uint256 idx) external view returns (uint256) {
+    function getOutcomePrice(uint256 idx) external view validIndex(idx) returns (uint256) {
+        if (_isResolved) return _resolvedTo == (idx == 0) ? 1e18 : 0;
         UD60x18[2] memory outcomeTotals = _getTotalSupplies();
         return outcomeTotals[idx].div(outcomeTotals[0] + outcomeTotals[1]).intoUint256();
     }
 
-    function getOutcomeLiquidity(uint256 idx) external view returns (uint256) {
+    function getOutcomeLiquidity(uint256 idx) external view validIndex(idx) returns (uint256) {
         return _outcomes[idx].totalSupply();
     }
 
@@ -80,17 +92,28 @@ contract TruthyMarket is IBinaryOutcomeMarket, Ownable {
         return _outcomes[0].totalSupply() + _outcomes[1].totalSupply();
     }
 
-    function buyOutcome(uint256 idx) external payable {}
+    function buyOutcome(uint256 idx) external payable notResolved validIndex(idx) {}
 
-    function redeemOutcome(uint256 idx, uint256 amount) external {}
+    function redeemOutcome(uint256 idx, uint256 amount) external validIndex(idx) {}
 
-    function resolve(uint256 idx) external onlyResolver {}
+    function resolve(uint256 idx) external onlyResolver notResolved validIndex(idx) {
+        // Store resolution status
+        _isResolved = true;
+        _resolvedTo = idx == 0;
+
+        // Burn the "dummy" outcome tokens held by this contract, representing positions on Polymarket
+        // After this, remaining tokens are held by users & winning outcome can be redeemed for $DEGEN
+        uint256[2] memory balancesThis = _getOutcomeBalances();
+        for (uint256 i; i < 2; ++i) {
+            _outcomes[i].burn(address(this), balancesThis[i]);
+        }
+    }
 
     /// @notice Update the outcome token balances of this contract to match updated prices on Polymarket
     /// @dev inputs should be determined by `newBalance[i] = totalMarketLiquidity * outcomePrice[i]`,
     ///      where `0 < outcomePrice[i] < 1 && outcomePrice[0] + outcomePrice[1] == 1`
     /// @param newBalances The new balances of the outcome tokens
-    function updatePolymarketBalances(uint256[2] memory newBalances) external onlyOwner {
+    function updatePolymarketBalances(uint256[2] memory newBalances) external onlyOwner notResolved {
         uint256[2] memory balancesThis = _getOutcomeBalances();
         for (uint256 i; i < 2; ++i) {
             if (newBalances[i] > balancesThis[i]) {
